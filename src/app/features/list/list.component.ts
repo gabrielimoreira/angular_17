67f70219ed081cf223a62b3127293b1955311d12
gabrieli.moreira <gabiiluisamoreira@gmail.com>
2025-02-28 13:56:48 -0300

    ✨ feat: Adiciona card dos produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
+import { MatCardModule} from '@angular/material/card'
+import { MatButtonModule} from '@angular/material/button'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
