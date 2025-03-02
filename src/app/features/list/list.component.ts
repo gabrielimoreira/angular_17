60b5916664883100262f1bbb619bcaf95eca6893
gabrieli.moreira <gabiiluisamoreira@gmail.com>
2025-03-02 00:21:46 -0300

    ♻ refactor: Melhora código de listComponent

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
 import { MatButtonModule} from '@angular/material/button'
 import { CardComponent } from './components/card/card.component';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
 
@@ -15,16 +15,10 @@ import { ConfirmationDialogService } from '../../shared/services/confirmation-di
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-  products: Product[] = [];
+  products = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
   productsService = inject(ProductsService);
   router = inject(Router);
-  confirmationDialogService = inject(ConfirmationDialogService)
-
-  ngOnInit(){
-    this.productsService.getAll().subscribe((products) => {
-      this.products = products;
-    });
-  }
+  confirmationDialogService = inject(ConfirmationDialogService);
 
   onEdit(product: Product){
     this.router.navigate(['/edit-product', product.id]);
@@ -35,10 +29,10 @@ export class ListComponent {
     .pipe(filter((answer) => answer === true))
     .subscribe(() => {
         this.productsService.delete(product.id).subscribe(()=>{
-        });
-        this.productsService.getAll().subscribe((products) => {
-          this.products = products;
-        });
+          this.productsService.getAll().subscribe((products) => {
+            this.products.set(products);
+          });
+        });        
       });
   }
 }
