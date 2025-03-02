3a7de9e707aa74765b66781c5487768d1fd561ce
gabrieli.moreira <gabiiluisamoreira@gmail.com>
2025-03-01 22:11:29 -0300

    ✨ feat: Adiciona pagina de edição

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,5 +13,12 @@ export const routes: Routes = [
             import('./features/create/create.component').then(
                 m => m.CreateComponent
             ),
+    },
+    {
+        path: 'edit-product',
+        loadComponent: () => 
+            import('./features/edit/edit.component').then(
+                m => m.EditComponent
+        ),
     }
 ];
