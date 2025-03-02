198358e5074e70450b55378f726f7c9edda315fb
gabrieli.moreira <gabiiluisamoreira@gmail.com>
2025-03-01 21:42:07 -0300

    ✨ feat: Adiciona Mensagem de Feedback ao criar pro

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -4,6 +4,8 @@ import { MatFormFieldModule} from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import {MatButtonModule} from '@angular/material/button'
 import { ProductsService } from '../../shared/services/products.service';
+import { MatSnackBar} from '@angular/material/snack-bar'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create',
   standalone: true,
@@ -13,6 +15,8 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class CreateComponent {
   productsService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
   
   form = new FormGroup({
     title: new FormControl<string>('', { 
@@ -25,7 +29,12 @@ export class CreateComponent {
       title: this.form.controls.title.value
     })
     .subscribe(() => {
-      alert('Sucesso!');
+      this.matSnackBar.open('Produto criado com sucesso!', 'Ok', {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top'
+      });
+      this.router.navigateByUrl('/');
     })
     
   }
